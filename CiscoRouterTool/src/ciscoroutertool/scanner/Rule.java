92dce5750cac5c5cdcb77bd9228ec537b63daf32
Andrew Johnston <ajohnston9@fordham.edu>
2014-07-17 09:03:52 -0400

    Added Rule class with matching algorithm.

---
+++ CiscoRouterTool/src/ciscoroutertool/scanner/Rule.java
@@ -0,0 +1,49 @@
+package ciscoroutertool.scanner;
+
+/**
+ * Holds the rules that config files are scanned against
+ * @author Andrew Johnston
+ */
+public class Rule {
+    
+    private String   name;
+    private String   description;
+    private String   severity;
+    private String[] settings;
+    private String[] params;
+    
+    public Rule(String _name, String _desc, String _sev, String[] _sett, String[] _params) {
+        name        = _name;
+        description = _desc;
+        severity    = _sev;
+        settings    = _sett;
+        params      = _params;
+    }
+    
+    public boolean matchesRule(String config) {
+        //Needs to match every rule in the ruleset
+        boolean[] matches = new boolean[settings.length];
+        String[] configLines = config.split("\n"); 
+        for (String line : configLines) {
+            for (int i = 0; i < settings.length; i++) {
+                String setting = settings[i];
+                //Since matches() must match whole string, this
+                //will ensure a good match succeeds
+                if (line.matches(setting + "(.*)")) {
+                    //Check if it matches the param
+                    if (line.matches("(.*)" + params[i])) {
+                        matches[i] = true;
+                    }
+                }
+            }
+        }
+        //Every rule must matches for it to be a match
+        boolean allTrue = true;
+        for (boolean b : matches) {
+            //AND the bools together, must all be true for allTrue to stay true
+            allTrue = allTrue && b;
+        }
+        return allTrue;
+    }
+    
+}
